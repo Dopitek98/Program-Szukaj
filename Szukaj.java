f62848b46233a1e6f5de553fded252c304446ca4
Dopitek98 <dopitek@wp.pl>
2021-08-25 12:12:57 +0200

    Wersja 1.1

--- Szukaj.java
+++ Szukaj.java
@@ -1,11 +1,12 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import javax.swing.*;
@@ -15,7 +16,6 @@ import java.util.List;
 
 public class Szukaj extends Application {
 
-
     public static void main(String[] args) {
         launch(args);
     }
@@ -24,26 +24,39 @@ public class Szukaj extends Application {
     public void start(Stage primaryStage) {
         TextField poletekstowe = new TextField();
         poletekstowe.setPromptText("Podaj slowo jakie chcesz znalesc w tekscie");
-        poletekstowe.setMinSize(100,50);
+        poletekstowe.setMinSize(400,50);
+        poletekstowe.setLayoutY(70);
+        poletekstowe.setLayoutX(10);
 
         TextField sciezka = new TextField();
-        sciezka.setPromptText("podaj sciezke do pliku");
-        sciezka.setMinSize(100,50);
-
+        sciezka.setPromptText("Podaj sciezke do pliku");
+        sciezka.setMinSize(400,50);
+        sciezka.setLayoutY(10);
+        sciezka.setLayoutX(10);
 
         ListView<String> listView = new ListView<>();
+        listView.setMinSize(730,500);
+        listView.setLayoutY(130);
+        listView.setLayoutX(10);
 
         Button restart = new Button("RESTART");
-        Button otworz = new Button("otworz plik");
+        restart.setMinSize(100,110);
+        restart.setLayoutY(10);
+        restart.setLayoutX(640);
+        Button otworz = new Button("Otworz plik");
+        otworz.setMinSize(100,110);
+        otworz.setLayoutY(10);
+        otworz.setLayoutX(420);
         Button szukajpliku = new Button("Szukaj pliku");
+        szukajpliku.setMinSize(100,110);
+        szukajpliku.setLayoutY(10);
+        szukajpliku.setLayoutX(530);
 
         restart.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-              //poletekstowe.setText(null);
                 poletekstowe.setText("");
               poletekstowe.setPromptText("Podaj slowo jakie chcesz znalesc w tekscie");
-              //sciezka.setText(null);
               sciezka.setText("");
               sciezka.setPromptText("Podaj sciezke do pliku");
               listView.getItems().remove(0,listView.getItems().size());
@@ -54,7 +67,7 @@ public class Szukaj extends Application {
             public void handle(ActionEvent event) {
                 JFileChooser znajdzplik = new JFileChooser();
                 znajdzplik.setCurrentDirectory(new File("C:\\Users"));
-                int udane = znajdzplik.showOpenDialog(null); // wybor pliku do otworzenia
+                int udane = znajdzplik.showOpenDialog(null);
                 if(udane==JFileChooser.APPROVE_OPTION){
                     File file = new File(znajdzplik.getSelectedFile().getAbsolutePath());
                     String path = file.getPath();
@@ -71,17 +84,14 @@ public class Szukaj extends Application {
                 try {
                     FileReader czytam = new FileReader(file);
                     BufferedReader buffer = new BufferedReader(czytam);
-                    System.out.println("podaj tekst");
 
                     Boolean zawiera = false;
                     String linia;
                     List<String> tablica = new ArrayList<>();
                     List<Integer> tab = new ArrayList<>();
                     List<String> strings = new ArrayList<>();
-                    String doWyniku = null;
                     int ilosc_znaków=0,numer_lini=1;
                     while((linia = buffer.readLine()) != null){
-                        System.out.println(linia);
                         ilosc_znaków+=linia.length();
                         numer_lini++;
                         if(linia.contains(tekst))
@@ -91,34 +101,27 @@ public class Szukaj extends Application {
                             zawiera = true;
                         }
                     }
-                    if(zawiera == true){
-                        System.out.println("\n zawiera dana fraze");
-                    }else if(zawiera == false){
-                        System.out.println("nie zawiera");
+                    if(zawiera == false){
                         listView.getItems().add(0,"Nie zawiera takiego znaku");
                     }
                     for(int i=0;i<tab.size();i++){
-                        System.out.println("Nr lini"+tab.get(i)+" "+tablica.get(i));
                         strings.add("Nr lini"+tab.get(i)+" "+tablica.get(i)+"\n");
                         listView.getItems().add(i,"Nr lini"+tab.get(i)+" "+tablica.get(i)+"\n");
                     }
                     buffer.close();
 
                 } catch (FileNotFoundException e) {
-                    //e.printStackTrace();
-                    System.out.println("Nie znaleziono pliku");
                     listView.getItems().add(0,"Nie znaleziono pliku");
                 }catch (IOException e){
-                    System.out.println("Problem z IO");
                     listView.getItems().add(0,"Problem z IO");
                 }catch (NullPointerException e){
-                    System.out.println("Wymagany restart programu");
                     listView.getItems().add(0,"Wymagany restart programu");
                 }
             }
         });
 
-        VBox grupa = new VBox();
+        Group grupa = new Group();
+
         grupa.getChildren().add(restart);
         grupa.getChildren().add(poletekstowe);
         grupa.getChildren().add(sciezka);
@@ -126,13 +129,11 @@ public class Szukaj extends Application {
         grupa.getChildren().add(szukajpliku);
         grupa.getChildren().add(listView);
 
-
-        Scene scene = new Scene(grupa,1280,720);
+        Scene scene = new Scene(grupa,750,640, Color.valueOf("4876FF"));
 
         primaryStage.setTitle("Program Szukaj");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
-
-
     }
 }
